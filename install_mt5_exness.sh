6a69e8e0eef5423e91c0f5006d9c60bdd7cdf0be
Syahdan Noor <syahdannoor@gmail.com>
2024-02-09 22:30:56 +0700

    Create install_mt5_exness.sh

---
+++ install_mt5_exness.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 1. download mt5 setup files from exness
+wget https://download.metatrader.com/cdn/web/exness.technologies.ltd/mt5/exness5setup.exe
+# 2. install mt5
+wine exness5setup.exe
